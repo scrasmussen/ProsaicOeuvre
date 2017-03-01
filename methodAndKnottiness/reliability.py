aee157ce27aa4f00a798b87e07583dc795265eb4
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-02-28 17:10:07 -0800

    Save point, got code written but need to figure out the base probabilities

--- methodAndKnottiness/reliability.py
+++ methodAndKnottiness/reliability.py
@@ -1,6 +1,10 @@
-import sys
+import sys, math
 
 count = 0
+cost=[]
+reliability=[]
+
+# Read input. Budget, number of machines, cost and reliability
 for line in sys.stdin:
     if count == 0:
         B = int(line)
@@ -8,10 +12,31 @@ for line in sys.stdin:
         N = int(line)
     else:
         c, r = line.rstrip().split(' ')
-        cost = int(c)
-        reliability = float(r)
-        
+        cost.append(int(c))
+        reliability.append(float(r))
+
     count+=1
 
+M = [[0 for i in range(B)] for i in range(N)]
+for i in range(B):
+    M[0][i]=1
+
+    
+print(cost)
+#for i in range(1,N):
+for i in range(1,3):
+    for b in range(0,B):
+        max = 0
+        # break
+        for k in range(0, math.floor(b/cost[i])):
+            m = M[i-1][b-k*cost[i]]*(1-reliability[i])**k
+            if m > max:
+                max = m
+                print("new max",max)
+
+print("Budget:", B)
+print("Number machines:", N)
+# print("\nIterated Version:")
+# print(M[0:3])
 
 print("Fin")
