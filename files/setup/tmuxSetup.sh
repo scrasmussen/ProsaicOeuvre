b1ba5f6137d93d736e8b440a753208867efc89cc
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-12-28 09:51:18 -0500

    added package manager and important packages

--- files/setup/tmuxSetup.sh
+++ files/setup/tmuxSetup.sh
@@ -1,9 +1,40 @@
 #!/usr/bin/env bash
-N=$'\n'
+# TODO:
+#  * make the command line into a function
+#  * look at google shell style guide
 
+N=$'\n'
+CONFIG="$HOME/.tmux.conf"
+
+# change tmux command to C-q
 tmux_prefix="set-option -g prefix C-q"
-if ! grep -q "$tmux_prefix" "$HOME/.tmux.conf"; then
-    echo "${N}${tmux_prefix}" >> "$HOME/.tmux.conf"
+if ! grep -q "^$tmux_prefix" "${CONFIG}"; then
+    echo "${N}${tmux_prefix}" >> "${CONFIG}"
+fi
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
+
+command="set -g @plugin 'tmux-plugins/tpm'"
+if ! grep -q "^$command" "${CONFIG}"; then
+    echo "${N}${command}" >> "${CONFIG}"
+fi
+
+command="set -g @plugin 'tmux-plugins/tmux-sensible'"
+if ! grep -q "^$command" "${CONFIG}"; then
+    echo "${N}${command}" >> "${CONFIG}"
+fi
+
+command="set -g @plugin 'tmux-plugins/tmux-yank'"
+if ! grep -q "^$command" "${CONFIG}"; then
+    echo "${N}${command}" >> "${CONFIG}"
+fi
+
+command="run -b ~/.tmux/plugins/tpm/tpm"
+if ! grep -q "^$command" "${CONFIG}"; then
+    echo "${N}# Init plugin manager. Keep at the bottom of tmux.conf" >> "${CONFIG}"
+    echo "${command}" >> "${CONFIG}"
 fi
 
 echo "Fin"
