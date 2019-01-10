256950d7b006a3c6a5ce337b82676a6183895770
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-01-10 16:27:43 +0000

    fixed append to end with sed

--- files/setup/bashSetup.sh
+++ files/setup/bashSetup.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
-N=$'\n'
 
 # set ding to visual
 ding="set bell-style visible"
 if ! grep -q "^$ding"  "/etc/inputrc"; then
-    echo "${N}${ding}" >> /etc/inputrc
+  sed -i -e "\$a$ding" /etc/inputrc
+
 fi
 
-BASHRC="${HOME}/.bashrc"
-PROMPT=$'PS1="C:\>"'
-if grep -q "^PS1=" "${BASHRC}"; then
-    echo "${N}${PROMPT}" >> "${BASHRC}"
+bashrc="${HOME}/.bashrc"
+prompt='PS1="C:\>"'
+if grep -q "^$prompt" "${bashrc}"; then
+  sed -i "1s/^/$prompt \n/" $bashrc
 fi
+
+# change less settings
+less_alias="alias less='less --silent --ignore-case --status-column'"
+if ! grep -q "^$less_alias" "${bashrc}"; then
+  sed -i -e "\$a$less_alias" $bashrc
+fi
+
 
 C=/mnt/c/Users/Artless/Documents
 U=$C/unix
