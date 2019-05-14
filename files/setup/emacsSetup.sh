e7c04836a576dd14f0d857b1230a49c478dc34ca
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-05-14 19:53:31 +0100

    lots of changes

--- files/setup/emacsSetup.sh
+++ files/setup/emacsSetup.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
 N=$'\n'
+config_file="$HOME/.emacs"
 
-# THIS IS INCOMPLETE
-# if ! grep -q "^ps stuff" "${HOME}/.emacs"; then
-#     echo "${N}"
-# fi
+function add(){
+  if ! grep -q "^$1" "${config_file}"; then
+    echo "${N}$1" >> ${config_file}
+  fi
+}
 
-# add (setq visible-bell 1)
+# add column and row numbers
+add "(setq column-number-mode t)"
+# highlight text beyond column 80
+add "(add-hook 'prog-mode-hook 'column-enforce-mode)"
+add "(add-hook 'before-save-hook 'delete-trailing-whitespace)"
+add "(setq visible-bell 1)"
 
+# handle autopair
+if ! grep -q "^(require 'autopair)"; then
+  url=https://raw.githubusercontent.com/capitaomorte/autopair/master/autopair.el
+  autopair_dir=~/.emacs.d/autopair
+  autopair_file=~/.emacs.d/autopair/autopair.el
+  [[ -d ${autopair_dir} ]] || mkdir -p ${autopair_dir}
+  [[ -f ${autopair_file} ]] || wget -P ${autopair_dir} ${url}
+  emacs -batch -f batch-byte-compile ${autopair_file}
+  add "(add-to-list 'load-path \"${autopair_dir}\")"
+  add "(require 'autopair)"
+  add "(autopair-global-mode)"
+fi
+
+
+# --- TODO ---
 # how to automatically do meta package-install?
-
 # fix indendation
 # https://www.emacswiki.org/emacs/IndentingC#toc1
