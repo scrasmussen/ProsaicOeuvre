ae978ac7b301dc92bd7bc8f840580491fde89ff4
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-05-14 20:12:49 +0100

    works better, adding 80 column rule

--- files/setup/emacsSetup.sh
+++ files/setup/emacsSetup.sh
@@ -3,6 +3,7 @@ N=$'\n'
 config_file="$HOME/.emacs"
 
 function add(){
+  echo $1
   if ! grep -q "^$1" "${config_file}"; then
     echo "${N}$1" >> ${config_file}
   fi
@@ -10,23 +11,31 @@ function add(){
 
 # add column and row numbers
 add "(setq column-number-mode t)"
-# highlight text beyond column 80
-add "(add-hook 'prog-mode-hook 'column-enforce-mode)"
 add "(add-hook 'before-save-hook 'delete-trailing-whitespace)"
 add "(setq visible-bell 1)"
 
-# handle autopair
-if ! grep -q "^(require 'autopair)"; then
-  url=https://raw.githubusercontent.com/capitaomorte/autopair/master/autopair.el
-  autopair_dir=~/.emacs.d/autopair
-  autopair_file=~/.emacs.d/autopair/autopair.el
-  [[ -d ${autopair_dir} ]] || mkdir -p ${autopair_dir}
-  [[ -f ${autopair_file} ]] || wget -P ${autopair_dir} ${url}
-  emacs -batch -f batch-byte-compile ${autopair_file}
-  add "(add-to-list 'load-path \"${autopair_dir}\")"
-  add "(require 'autopair)"
-  add "(autopair-global-mode)"
-fi
+# setup autopair
+url=https://raw.githubusercontent.com/capitaomorte/autopair/master/autopair.el
+autopair_dir=~/.emacs.d/autopair
+autopair_file=~/.emacs.d/autopair/autopair.el
+[[ -d ${autopair_dir}  ]] || mkdir -p ${autopair_dir}
+[[ -f ${autopair_file} ]] || wget -P ${autopair_dir} ${url}
+emacs -batch -f batch-byte-compile ${autopair_file}
+add "(add-to-list 'load-path \"${autopair_dir}\")"
+add "(require 'autopair)"
+add "(autopair-global-mode)"
+
+# setup highlighting text beyond column 80
+url=https://raw.githubusercontent.com/jordonbiondo/column-enforce-mode/master/column-enforce-mode.el
+columnEnforce_dir=~/.emacs.d/columnEnforce
+columnEnforce_file=~/.emacs.d/columnEnforce/column-enforce-mode.el
+[[ -d ${columnEnforce_dir}  ]] || mkdir -p ${columnEnforce_dir}
+[[ -f ${columnEnforce_file} ]] || wget -P ${columnEnforce_dir} ${url}
+emacs -batch -f batch-byte-compile ${columnEnforce_file}
+add "(add-to-list 'load-path \"${columnEnforce_dir}\")"
+add "(add-hook 'prog-mode-hook 'column-enforce-mode)"
+
+
 
 
 # --- TODO ---
