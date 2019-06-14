a9a8074096e033bba3b52cde2e62124dbec7ee6e
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-06-14 22:40:14 +0100

    added cmake

--- files/setup/aptInstall.sh
+++ files/setup/aptInstall.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e -x
 if [ "$EUID" -ne 0 ]
 then echo "Please run as root to install packages"
      exit
@@ -8,3 +9,6 @@ sudo apt update
 sudo apt install emacs25-nox
 sudo apt install tmux
 sudo apt install x11-xkb-utils
+sudo apt install build-essential
+sudo apt install autoconf
+sudo apt install cmake
