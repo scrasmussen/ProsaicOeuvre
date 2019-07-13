8f131196c542e6f0455e79f1f9f6e55d4ce268a6
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-07-13 12:12:52 -0700

    updating these files with org-mode stuff and others

--- files/setup/aptInstall.sh
+++ files/setup/aptInstall.sh
@@ -6,9 +6,16 @@ then echo "Please run as root to install packages"
 fi
 
 sudo apt update
+sudo apt upgrade
 sudo apt install emacs25-nox
 sudo apt install tmux
 sudo apt install x11-xkb-utils
 sudo apt install build-essential
 sudo apt install autoconf
 sudo apt install cmake
+sudo apt install python3
+sudo apt install pip3
+
+pip3 install ipython
+pip3 install numpy
+pip3 install pandas
