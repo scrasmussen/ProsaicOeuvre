92bf71f7f5f0597cb05ee9583644aa826c5204a1
Soren Rasmussen <s.c.rasmussen@gmail.com>
2024-09-16 13:57:44 -0600

    updating major modes

--- src/setup/emacsPackage.sh
+++ src/setup/emacsPackage.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-
+# === misc. ===
 # imenu-list
-# modes
+# org-drill
+
+# === modes ===
+# cmake-mode
+# dockerfile-mode
+# git-modes
+# js2-mode
+# julia-mode
+# ledger-mode
+# lua-mode
+# markdown-mode
+# mermaid-mode
+# yaml-mode
